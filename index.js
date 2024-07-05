c4db44481690c55de228757bc885a3ce70581ca4
Slindile45 <168456312+Slindile45@users.noreply.github.com>
2024-07-05 12:06:48 +0200

    added basic js

---
+++ index.js
@@ -0,0 +1,14 @@
+function updateTime() {
+  let durbanElement = document.querySelector("#Durban");
+  let durbanDateElement = durbanElement.querySelector(".date");
+  let durbanTimeElement = durbanElement.querySelector(".time");
+  let durbanTime = moment().tx("Africa/Johannesburg");
+
+  durbanDateElement.innerHTML = durbanTime.format("MMMM Do YYYY");
+  durbanTimeElement.innerHTML = durbanTime.format(
+    "HH:mm:ss [<small>]A[</small>]"
+  );
+}
+
+updateTime();
+setInterval(updateTime, 1000);
